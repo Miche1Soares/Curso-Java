f2c47b82b012b6c9c001cd565526154da36a82cf
Michel <miche1ss433@gmail.com>
2023-02-11 00:44:22 -0300

    a21

---
+++ a21/src/application/App.java
@@ -0,0 +1,36 @@
+/*
+
+https://snipboard.io/EamvIG.jpg
+https://snipboard.io/hKbWnL.jpg
+
+*/
+
+package application;
+
+import java.text.SimpleDateFormat;
+
+import entities.Comment;
+import entities.Post;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+
+        Comment c1 = new Comment("Have a nice trip");
+        Comment c2 = new Comment("Wow that's awesome!");
+
+        Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),
+        "Traveling to New Zealand",
+        "I'm going to visit this wonderful country!",
+        12);
+
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        System.out.println(p1);
+
+    }
+}
