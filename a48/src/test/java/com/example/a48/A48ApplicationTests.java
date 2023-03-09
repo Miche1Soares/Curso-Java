185b5091d61693bf9c915c072323c5d4bdacaa5a
Michel <miche1ss433@gmail.com>
2023-03-08 21:59:47 -0300

    a48

---
+++ a48/src/test/java/com/example/a48/A48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
