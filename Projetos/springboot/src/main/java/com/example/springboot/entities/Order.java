672cce0ad7816116982390e519eb5c0e7ab1a280
Michel <miche1ss433@gmail.com>
2023-03-21 23:00:51 -0300

    Category and Product

--- Projetos/springboot/src/main/java/com/example/springboot/entities/Order.java
+++ Projetos/springboot/src/main/java/com/example/springboot/entities/Order.java
@@ -2,6 +2,7 @@ package com.example.springboot.entities;
 
 import java.time.Instant;
 
+import com.example.springboot.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 //import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -32,6 +33,9 @@ public class Order implements Serializable{
     private Instant moment;
 
 
+    private Integer orderStatus;
+
+
     // impede o loop das relaçoes das tabelas (pois uma chama a outra sem parar)
     //@JsonIgnore
     // muitos pedidos para 1 usuário
@@ -42,10 +46,11 @@ public class Order implements Serializable{
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client)
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client)
     {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client; 
     }
 
@@ -63,6 +68,17 @@ public class Order implements Serializable{
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null)
+        {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
