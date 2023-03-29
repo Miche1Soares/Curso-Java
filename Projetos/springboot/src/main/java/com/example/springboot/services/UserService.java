b38bdbc974f1c95c70ebde8b0b0291889d557206
Michel <miche1ss433@gmail.com>
2023-03-28 21:37:52 -0300

    insert user, delete and update exceptions

--- Projetos/springboot/src/main/java/com/example/springboot/services/UserService.java
+++ Projetos/springboot/src/main/java/com/example/springboot/services/UserService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.springboot.entities.User;
 import com.example.springboot.repositories.UserRepository;
+import com.example.springboot.services.exceptions.DatabaseException;
+import com.example.springboot.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 // para o autowired do userresource funcionar
 @Service
@@ -24,7 +30,7 @@ public class UserService {
     public User findById(Long id)
     {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
 
@@ -38,17 +44,35 @@ public class UserService {
     // deletando um usuario
     public void delete(Long id)
     {
-        repository.deleteById(id);
+        try
+        {
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e)
+        {
+            throw new ResourceNotFoundException(id);
+        }
+        catch(DataIntegrityViolationException e)
+        {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
 
     // atualizando um usuario
     public User update(Long id, User obj)
     {
-        //                       diferente do findById (que vai no banco de dados e traz o "objeto") o getReferenceById só prepara um objeto para realizar alterações e dps enviar ao database
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try 
+        {
+            //                       diferente do findById (que vai no banco de dados e traz o "objeto") o getReferenceById só prepara um objeto para realizar alterações e dps enviar ao database
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e)
+        {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) 
